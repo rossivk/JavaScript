8abc308c846b3f3acb8eb560fe868e473c5889f0
Rosica Kirkova <96499964+rossivk@users.noreply.github.com>
2022-05-22 23:57:37 +0100

    Update listOfProducts.js

--- listOfProducts.js
+++ listOfProducts.js
@@ -1,6 +1,7 @@
 function listOfProducts(products) {
 
-  products.sort((a, b) => a.localeCompare(b));
+  products.sort();
+//   products.sort((a, b) => a.localeCompare(b));
 
   for (let i = 0; i < products.length; i++) {
     console.log(`${i + 1}.${products[i]}`);
