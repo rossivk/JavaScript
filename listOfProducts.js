822dd43d040d3c2933765d6130e9758679ed6974
Rosica Kirkova <96499964+rossivk@users.noreply.github.com>
2022-05-22 23:49:07 +0100

    Create listOfProducts.js
    List of Products
    You will receive an array of products. Print a numbered array of all the products ordered by name.
    ['Potatoes', 'Tomatoes', 'Onions', 'Apples']	1.Apples
    2.Onions
    3.Potatoes
    4.Tomatoes
    ['Watermelon', 'Banana', 'Apples']	1.Apples
    2.Banana
    3.Watermelon

---
+++ listOfProducts.js
@@ -0,0 +1,9 @@
+function listOfProducts(products) {
+
+  products.sort((a, b) => a.localeCompare(b));
+
+  for (let i = 0; i < products.length; i++) {
+    console.log(`${i + 1}.${products[i]}`);
+  }
+  
+}
