100fcf7cb67643f13766462203f7f9f9258c4e4e
Rosica Kirkova <96499964+rossivk@users.noreply.github.com>
2022-06-16 22:44:24 +0100

    Create schoolGrades.js
    School Grades
    Write a function that stores students and their grades throughout the year. If a student appears more than once, add the new grades to existing ones. Finally, print the students and their average grades, sorted alphabetically by student name. The input comes as an array of strings.
    Note: The average grades must be fixed to the second decimal place.
    Example
    Input	Output
    ['Lilly 4 6 6 5',
    'Tim 5 6',
    'Tammy 2 4 3',
    'Tim 6 6']	Lilly: 5.25
    Tammy: 3.00
    Tim: 5.75
    ['Steven 3 5 6 4',
    'George 4 6',
    'Tammy 2 5 3',
    'Steven 6 3']	George: 5.00
    Steven: 4.50
    Tammy: 3.33

---
+++ schoolGrades.js
@@ -0,0 +1,38 @@
+function schoolGrades(array) {
+    const grades = {};
+
+    array.forEach(line => {
+        let personArray = line.split(' ');
+        let firstName = personArray.splice(0, 1);
+
+        if (grades.hasOwnProperty(firstName)) {
+            const previousGrades = grades[firstName];
+            grades[firstName] = previousGrades.concat(personArray);
+        } else {
+            grades[firstName] = personArray;
+        }
+    });
+
+    const sortedGrades = Object.fromEntries(Object.entries(grades).sort());
+
+    for (const kvp of Object.entries(sortedGrades)) {
+        let totalGrades = 0;
+        let avarageGrade = 0;
+        for (let index = 0; index < kvp[1].length; index++) {
+            totalGrades += Number((kvp[1])[index]);
+            avarageGrade = totalGrades / kvp[1].length;
+        }
+        console.log(`${kvp[0]}: ${avarageGrade.toFixed(2)}`);
+    }
+
+}
+
+
+
+schoolGrades(
+    ['Steven 3 5 6 4',
+        'George 4 6',
+        'Tammy 2 5 3',
+        'Steven 6 3']
+
+)
